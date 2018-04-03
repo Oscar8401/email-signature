57589470fb3827ab52f12b804e0e839a2982bb65
Surasak Prommarat <surasak@adapterdigital.com>
2018-04-03 12:56:07 +0700

    update email signature

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	  <div class="container">
+	  	<app-header title="Adapter Email Signature"/>
+			<router-view></router-view>
+		</div>
+	</div>
 </template>
 
 <script>
+import { Header } from './components/index'
 export default {
-  name: 'App'
-}
+	components: {
+			appHeader:Header
+	},
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
